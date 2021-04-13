d812c8cdfbce08ebe296b5eca8b39357f9f45827
Waliff Cordeiro <waliffcordeiro@gmail.com>
2021-04-13 20:55:20 -0300

    Order by ArrivalTime and Duration

--- main.go
+++ main.go
@@ -8,8 +8,8 @@ import (
 
 func main() {
 	processList, _ := processes.Parse("./file.txt")
-	processExecutionSjf, _ := processes.Sjf(processList)
 	processExecutionFifo, _ := processes.Fifo(processList)
+	processExecutionSjf, _ := processes.Sjf(processList)
 	fmt.Println(processList)
 	fmt.Println(processExecutionSjf)
 	fmt.Println(processExecutionFifo)
--- processes/sjf.go
+++ processes/sjf.go
@@ -6,8 +6,14 @@ import (
 
 func Sjf(processes []Process) ([]ProcessExecution, error) {
 
-	// Ordenando com base na menor duração
-	sort.Slice(processes, func(i, j int) bool { return processes[i].duration < processes[j].duration })
+	// Ordenando com base na menor duração, levando em consideração a ordem
+	sort.Slice(processes, func(i, j int) bool {
+		if processes[i].arrivalTime != processes[j].arrivalTime {
+			return processes[i].arrivalTime < processes[j].arrivalTime
+		}
+
+		return processes[i].duration < processes[j].duration
+	})
 
 	return Fifo(processes)
 }
