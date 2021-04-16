f9d65cf131d177ae2b2424737e866678db65c950
PedroAugustoRamalhoDuarte <pedro_aduarte@aluno.unb.br>
2021-04-15 21:43:59 -0300

    Debug file

--- main.go
+++ main.go
@@ -10,6 +10,8 @@ func main() {
 	processList, _ := processes.Parse("./file.txt")
 	processExecutionFifo, _ := processes.Fifo(processList)
 	processExecutionSjf, _ := processes.Sjf(processList)
+	processes.ExecutionToFile(processExecutionFifo, "fifo.txt")
+	processes.ExecutionToFile(processExecutionFifo, "sjf.txt")
 	fmt.Println(processList)
 	fmt.Println(processExecutionSjf)
 	fmt.Println(processExecutionFifo)
---
+++ processes/formatter.go
@@ -0,0 +1,25 @@
+package processes
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+func ExecutionToFile(processesExecutionList []ProcessExecution, filename string) {
+	f, err := os.Create(filename)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer f.Close()
+
+	for _, processExecution := range processesExecutionList {
+		_, err := f.WriteString(fmt.Sprintf("Rodar processo [%d] de [%d] ate [%d]\n",
+			processExecution.pid, processExecution.startTime, processExecution.finishTime))
+		if err != nil {
+			return
+		}
+	}
+}
