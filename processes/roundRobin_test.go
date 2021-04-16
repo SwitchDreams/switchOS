2e50da533bab8b3dfa5fc2f22f79d24378539643
PedroAugustoRamalhoDuarte <pedro_aduarte@aluno.unb.br>
2021-04-15 22:03:26 -0300

    Test for round robin with duration no multiple of 2

--- processes/roundRobin_test.go
+++ processes/roundRobin_test.go
@@ -44,3 +44,31 @@ func TestRoundRobin(t *testing.T) {
 		t.Errorf("got %v, wanted %v", got, want)
 	}
 }
+
+// TODO: Pass in this test
+func TestRoundRobinDurationNotMod2(t *testing.T) {
+	processes := []Process{
+		{id: 1, arrivalTime: 0, duration: 6},
+		{id: 2, arrivalTime: 0, duration: 3},
+		{id: 3, arrivalTime: 0, duration: 1},
+		{id: 4, arrivalTime: 0, duration: 7},
+	}
+
+	processesExecution := []ProcessExecution{
+		{pid: 1, startTime: 0, finishTime: 2},
+		{pid: 2, startTime: 2, finishTime: 4},
+		{pid: 3, startTime: 4, finishTime: 5},
+		{pid: 4, startTime: 5, finishTime: 7},
+		{pid: 1, startTime: 7, finishTime: 9},
+		{pid: 2, startTime: 9, finishTime: 10},
+		{pid: 4, startTime: 10, finishTime: 12},
+		{pid: 1, startTime: 12, finishTime: 14},
+		{pid: 4, startTime: 16, finishTime: 17},
+	}
+	got, _ := RoundRobin(processes)
+	want := processesExecution
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("\ngot    %v \nwanted %v", got, want)
+	}
+}
