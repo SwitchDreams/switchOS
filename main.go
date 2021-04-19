2fde10b8fd95635ca8e895fe94ce36d58d0df0a7
joaopedroassis <joaopedroassisdossantos@gmail.com>
2021-04-18 21:05:39 -0300

    🥅 handle errors in main

--- main.go
+++ main.go
@@ -10,20 +10,45 @@ import (
 
 func main() {
 
+	// Read input file
 	inputFile := os.Args[1]
-	processList, _ := p.Parse(inputFile)
+	processList, err := p.Parse(inputFile)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
-	processExecutionFifo, _ := algorithms.Fifo(processList)
-	processExecutionSjf, _ := algorithms.Sjf(processList)
-	processExecutionRoundRobin, _ := algorithms.Sjf(processList)
+	// Run algorithms
+	processExecutionFifo := algorithms.Fifo(processList)
+	processExecutionSjf := algorithms.Sjf(processList)
+	processExecutionRoundRobin := algorithms.RoundRobin(processList)
+
+	// Get average time
 	ttTime, responseTime, waitTime := p.AverageTime(processList, processExecutionFifo, false)
 	fmt.Printf("FIFO %.1f %.1f %.1f\n", ttTime, responseTime, waitTime)
+
 	ttTime, responseTime, waitTime = p.AverageTime(processList, processExecutionSjf, false)
 	fmt.Printf("SJF %.1f %.1f %.1f\n", ttTime, responseTime, waitTime)
+
 	ttTime, responseTime, waitTime = p.AverageTime(processList, processExecutionRoundRobin, true)
 	fmt.Printf("RR %.1f %.1f %.1f\n", ttTime, responseTime, waitTime)
-	p.ExecutionToFile(processExecutionFifo, "fifo.txt")
-	p.ExecutionToFile(processExecutionSjf, "sjf.txt")
-	p.ExecutionToFile(processExecutionRoundRobin, "rr.txt")
+
+	err = p.ExecutionToFile(processExecutionFifo, "fifo.txt")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	err = p.ExecutionToFile(processExecutionSjf, "sjf.txt")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	err = p.ExecutionToFile(processExecutionRoundRobin, "rr.txt")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
 }
