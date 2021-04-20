74139dddae64cdbf1ea3df19cf3c66a7f08d7566
PedroAugustoRamalhoDuarte <pedro_aduarte@aluno.unb.br>
2021-04-19 21:05:27 -0300

    Fifo memory algorithm closes #12

--- memory/algorithms/fifo.go
+++ memory/algorithms/fifo.go
@@ -6,5 +6,24 @@ import (
 
 // Fifo implements the First In First Out algorithm
 func Fifo(memory m.Memory) int {
-	return 10
+	var ram = make([]int, memory.Size)
+	var faults int
+	var cont int
+	for _, page := range memory.Sequence {
+		if !find(ram, page) {
+			faults += 1
+			ram[cont%memory.Size] = page
+			cont += 1
+		}
+	}
+	return faults
+}
+
+func find(slice []int, val int) bool {
+	for _, item := range slice {
+		if item == val {
+			return true
+		}
+	}
+	return false
 }
--- memory/algorithms/fifo_test.go
+++ memory/algorithms/fifo_test.go
@@ -19,3 +19,16 @@ func TestFifo(t *testing.T) {
 		t.Errorf("got %v, wanted %v", got, want)
 	}
 }
+
+func TestFifoSize3(t *testing.T) {
+	memory := m.Memory{
+		Size:     3,
+		Sequence: []int{1, 2, 3, 4, 1, 2, 5, 1, 2, 3, 4, 5},
+	}
+	got := Fifo(memory)
+	want := 9
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v, wanted %v", got, want)
+	}
+}
