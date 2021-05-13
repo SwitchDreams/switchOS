252141a10d1999a627823c8ecd83c9a969fcc59e
joaopedroassis <joaopedroassisdossantos@gmail.com>
2021-05-12 21:44:30 -0300

    adds second chance

--- memory/algorithms/secondChance.go
+++ memory/algorithms/secondChance.go
@@ -2,9 +2,24 @@ package algorithms
 
 import (
 	m "github.com/switchdreams/switchOS/memory"
+	"github.com/switchdreams/switchOS/utils"
 )
 
 // SecondChance implements the FIFO variation
 func SecondChance(memory m.Memory) int {
-	return 10
+	ram := make([]int, memory.Size)
+	var faults, cont int
+
+	for _, page := range memory.Sequence {
+		if !utils.Find(ram, page.Page) {
+			if !page.R {
+				page.R = true
+			} else {
+				faults++
+				ram[cont%memory.Size] = page.Page
+				cont++
+			}
+		}
+	}
+	return faults
 }
