4a2d814da44a972532e963a17bbedb3ae0ea3f44
PedroAugustoRamalhoDuarte <pedro_aduarte@aluno.unb.br>
2021-05-13 19:54:10 -0300

    Tests for ssf

--- io/algorithms/ssf_test.go
+++ io/algorithms/ssf_test.go
@@ -10,6 +10,20 @@ import (
 func TestSSF(t *testing.T) {
 	disk := io.Disk{
 		Size:     199,
+		Init:     53,
+		Sequence: []int{98, 183, 37, 122, 14, 124, 65, 67},
+	}
+	got := Scan(disk)
+	want := 236
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v, wanted %v", got, want)
+	}
+}
+
+func TestSSFGeek(t *testing.T) {
+	disk := io.Disk{
+		Size:     199,
 		Init:     50,
 		Sequence: []int{82, 170, 43, 140, 24, 16, 190},
 	}
