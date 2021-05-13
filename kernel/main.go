c839d1dcca6a282d50c5ca190d94337a3e3cda96
joaopedroassis <joaopedroassisdossantos@gmail.com>
2021-05-12 22:46:46 -0300

    adds error handling

--- kernel/main.go
+++ kernel/main.go
@@ -2,33 +2,51 @@ package kernel
 
 import (
 	"fmt"
+
+	"github.com/switchdreams/switchOS/errors"
+	io "github.com/switchdreams/switchOS/io"
+	ioAlgorithms "github.com/switchdreams/switchOS/io/algorithms"
 	m "github.com/switchdreams/switchOS/memory"
 	mAlgorithms "github.com/switchdreams/switchOS/memory/algorithms"
 	p "github.com/switchdreams/switchOS/processes"
 	pAlgorithms "github.com/switchdreams/switchOS/processes/algorithms"
 )
 
-const PROCESS = 1
-const MEMORY = 2
-const IO = 3
+const (
+	PROCESS = 1
+	MEMORY  = 2
+	IO      = 3
+)
 
-func Exec(module int, fileName string) {
+func Exec(module int, filename string) errors.IOSError {
 
 	switch module {
 	case PROCESS:
-		handleProcess(fileName)
+		err := handleProcess(filename)
+		if err != nil {
+			return errors.WrapError(err, "failed to handle processes")
+		}
 	case MEMORY:
-		handleMemory(fileName)
+		err := handleMemory(filename)
+		if err != nil {
+			return errors.WrapError(err, "failed to handle memory")
+		}
 	case IO:
+		err := handleIO(filename)
+		if err != nil {
+			return errors.WrapError(err, "failed to handle IO")
+		}
+	default:
+		return errors.NewOSError("Opção não reconhecida. As opções disponíveis são 1, 2 ou 3")
 	}
 
+	return nil
 }
 
-func handleProcess(fileName string) {
-	processList, err := p.Parse(fileName)
+func handleProcess(filename string) errors.IOSError {
+	processList, err := p.Parse(filename)
 	if err != nil {
-		fmt.Println(err)
-		return
+		return errors.WrapError(err, fmt.Sprintf("failed to parse %s", filename))
 	}
 
 	// Run algorithms
@@ -48,28 +66,26 @@ func handleProcess(fileName string) {
 
 	err = p.ExecutionToFile(processExecutionFifo, "fifo.txt")
 	if err != nil {
-		fmt.Println(err)
-		return
+		return errors.WrapError(err, "failed to print fifo.txt")
 	}
 
 	err = p.ExecutionToFile(processExecutionSjf, "sjf.txt")
 	if err != nil {
-		fmt.Println(err)
-		return
+		return errors.WrapError(err, "failed to print sjf.txt")
 	}
 
 	err = p.ExecutionToFile(processExecutionRoundRobin, "rr.txt")
 	if err != nil {
-		fmt.Println(err)
-		return
+		return errors.WrapError(err, "failed to print rr.txt")
 	}
+
+	return nil
 }
 
-func handleMemory(fileName string) {
-	memory, err := m.Parse(fileName)
+func handleMemory(filename string) errors.IOSError {
+	memory, err := m.Parse(filename)
 	if err != nil {
-		fmt.Println(err)
-		return
+		return errors.WrapError(err, fmt.Sprintf("failed to parse %s", filename))
 	}
 	// Run algorithms
 	fifoFaults := mAlgorithms.Fifo(memory)
@@ -79,4 +95,23 @@ func handleMemory(fileName string) {
 	fmt.Printf("FIFO %d\n", fifoFaults)
 	fmt.Printf("SC %d\n", secondChanceFaults)
 	fmt.Printf("LRU %d\n", lruFaults)
+
+	return nil
+}
+
+func handleIO(filename string) errors.IOSError {
+	disk, err := io.Parse(filename)
+	if err != nil {
+		return errors.WrapError(err, fmt.Sprintf("failed to parse %s", filename))
+	}
+
+	fcfsSeekTime := ioAlgorithms.Fcfs(disk)
+	ssfSeekTime := ioAlgorithms.SSF(disk)
+	scanSeekTime := ioAlgorithms.Scan(disk)
+
+	fmt.Printf("FCFS %d\n", fcfsSeekTime)
+	fmt.Printf("SSF %d\n", ssfSeekTime)
+	fmt.Printf("SCAN %d\n", scanSeekTime)
+
+	return nil
 }
