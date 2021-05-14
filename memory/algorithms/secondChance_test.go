55c92fc9e0979c7f633b4d9d6b4b54cb8728881c
joaopedroassis <joaopedroassisdossantos@gmail.com>
2021-05-14 00:48:43 -0300

    adds test
    Co-authored-by: Waliff Cordeiro <waliffcordeiro@gmail.com>

--- memory/algorithms/secondChance_test.go
+++ memory/algorithms/secondChance_test.go
@@ -23,10 +23,10 @@ func TestSecondChance(t *testing.T) {
 func TestSecondChanceGeek(t *testing.T) {
 	memory := m.Memory{
 		Size:     3,
-		Sequence: []int{2, 5, 10, 1, 2, 2, 6, 9, 1, 2, 10, 2, 6, 1, 2, 1, 6, 9, 5, 1, 3},
+		Sequence: []int{7, 0, 1, 2, 0, 3, 0, 4, 2, 4, 0, 3, 2},
 	}
 	got := SecondChance(memory)
-	want := 13
+	want := 8
 
 	if !reflect.DeepEqual(got, want) {
 		t.Errorf("got %v, wanted %v", got, want)
